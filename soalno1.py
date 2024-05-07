00014da0feede9f78b7619774abeda99ad566523
RAFLINUR <145580383+RAFLINUR@users.noreply.github.com>
2024-05-07 16:36:14 +0700

    first commit

---
+++ soalno1.py
@@ -0,0 +1,8 @@
+print('start')
+input_bilangan_pertama = 50
+input_bilangan_kedua = 35
+tampilkan_hasil_penjumlahan = input_bilangan_pertama + input_bilangan_kedua
+tampilkan_hasil_pengurangan = input_bilangan_pertama - input_bilangan_kedua
+print(tampilkan_hasil_penjumlahan)
+print(tampilkan_hasil_pengurangan)
+print('end')
